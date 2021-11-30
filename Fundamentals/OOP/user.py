a1a80e02304d2003b2d734060c08a64a734ec548
Jaden Willeiksen <perryjaden@yahoo.com>
2021-11-30 12:10:31 -0800

    initial

--- Fundamentals/OOP/user.py
+++ Fundamentals/OOP/user.py
@@ -5,41 +5,33 @@ class User:		# here's what we have so far
         self.account_balance = 0
     # adding the deposit method
 
-    def make_deposit(self, amount):  # takes an argument that is the amount of the deposit
-    	# the specific user's account increases by the amount of the value received
-    	self.account_balance += amount
+    def make_deposit(self, amount):  # takes an argument that is the amount of the deposit, the specific user's account increases by the amount of the value received
+        self.account_balance += amount
+        return self
     
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
     
     def display_user_balance(self):
         print(self.account_balance)
+        return self
     
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
+        return self
 
 
 guido = User("Guido van Rossum", "guido@python.com")
 monty = User("Monty Python", "monty@python.com")
 jaden = User("Jaden Willeiksen", "email@email.com")
 
-guido.make_deposit(100)
-guido.make_deposit(200)
-guido.make_deposit(20)
-print(guido.account_balance) #320
+guido.make_deposit(100).make_deposit(200).make_deposit(20).transfer_money(jaden, 100).display_user_balance()
 
-monty.make_deposit(700)
-monty.make_deposit(50)
-monty.make_withdrawal(50)
-print(monty.account_balance) #700
+monty.make_deposit(700).make_deposit(50).make_withdrawal(50).display_user_balance()
 
-jaden.make_deposit(5000)
-jaden.make_withdrawal(1000)
-jaden.make_withdrawal(1000)
-jaden.make_withdrawal(1000)
-print(jaden.account_balance) #2000
+jaden.make_deposit(5000).make_withdrawal(1000).make_withdrawal(1000).make_withdrawal(1000).display_user_balance()
 
-guido.transfer_money(jaden, 100)
 print(guido.account_balance) #220
 print(jaden.account_balance) #2100

bf74b8bf3062cd3a3fcca917d6dac571da97f222
Jaden Willeiksen <perryjaden@yahoo.com>
2021-12-10 11:58:35 -0800

    initial

--- Flask/Fundamentals/dojo_survey/server.py
+++ Flask/Fundamentals/dojo_survey/server.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash
 
 app = Flask(__name__)
 app.secret_key = 'dojosurvey'
 
 
 @app.route('/')
-def countpage():
+def surveypage():
     return render_template("index.html")
 
 
 @app.route('/process', methods=['POST'])
 def process():
+    if not validate_form(request.form):
+        return redirect('/')
     session['name'] = request.form['name']
     session['location'] = request.form['location']
     session['language'] = request.form['language']
@@ -21,6 +23,24 @@ def process():
 @app.route('/result')
 def result():
     return render_template("result.html", info=session)
+
+
+@staticmethod
+def validate_form(form):
+    is_valid = True
+    if len(form['name']) < 2:
+        flash("Name must be at least 2 characters.")
+        is_valid = False
+    if len(form['location']) < 7:
+        flash("Please select a valid location.")
+        is_valid = False
+    if len(form['language']) < 4:
+        flash("Please select a valid language.")
+        is_valid = False
+    if len(form['comment']) > 250:
+        flash("Your comment is too long. 250 characters maximum.")
+        is_valid = False
+    return is_valid
 
 if __name__ == "__main__":
     app.run(debug=True)
